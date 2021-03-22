7cc1a409737ae12bd6cab5f93b8afc88572485ed
wellington <wellingtonslpbi@hotmail.com>
2021-03-22 10:28:00 -0300

    add global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import Head from 'next/head'
 
-import styles from '../styles/home.module.scss'
-
 export default function Home() {
   return (
     <>
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      <h1 className={styles.title}>Hello World</h1>
+      <h1>Hello World</h1>
     </>
   )
 }
