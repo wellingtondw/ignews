df9ac49665283840f41f25f49125c9d307e59a39
wellington <wellingtonslpbi@hotmail.com>
2021-03-25 18:34:46 -0300

    create active link

---
+++ src/components/ActiveLink/index.tsx
@@ -0,0 +1,20 @@
+import { useRouter } from 'next/router'
+import Link, { LinkProps } from "next/link";
+import { ReactElement, cloneElement } from "react";
+
+type ActiveLinkProps = {
+  children: ReactElement
+  activeClassName: string;
+} & LinkProps
+
+export function ActiveLink({ children, activeClassName, ...props }: ActiveLinkProps) {
+  const { asPath } = useRouter()
+
+  const className = asPath === props.href ? activeClassName : ''
+
+  return (
+    <Link {...props}>
+      {cloneElement(children, { className })}
+    </Link>
+  )
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,20 @@
-import Link from 'next/link'
+import { ActiveLink } from '../ActiveLink'
 import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
 
 export function Header() {
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news"/>
         <nav>
-          <Link href='/'>
-            <a className={styles.active}>Home</a>
-          </Link>
-          <Link href='/posts'>
+          <ActiveLink activeClassName={styles.active} href='/' >
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href='/posts'>
             <a>Posts</a>
-          </Link>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
