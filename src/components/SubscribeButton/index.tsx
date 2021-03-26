ce85a41d82832e45311b9c32bd2980b4d1bb1073
wellington <wellingtonslpbi@hotmail.com>
2021-03-26 09:37:39 -0300

    validating signature

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,5 @@
 import { signIn, useSession } from 'next-auth/client'
+import { useRouter } from 'next/router'
 import { api } from '../../services/api'
 import { getStripeJs } from '../../services/stripe-js'
 import styles from './styles.module.scss'
@@ -9,11 +10,20 @@ type SubscribeButtonProps = {
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession()
+  const router = useRouter()
 
   async function handleSubscribe() {
     if(!session) {
       signIn('github')
       return;
+    }
+
+    // console.log(session)
+
+    if(session.activeSubscription) {
+      router.push('/posts');
+
+      return
     }
 
     try {
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -37,11 +37,17 @@ export default function Post({ post }: PostProps) {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
-  const session = getSession({ req })
+  const session = await getSession({ req })
   const { slug } = params
 
-  // if(!session) {
-  // }
+  if(!session.activeSubscription) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
 
   const prismic = getPrismicClient(req)
 
